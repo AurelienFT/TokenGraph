647badc73732cfd71771b41051e5f659175e0a9f
AurelienFT <aurelien.foucault@epitech.eu>
2021-11-02 18:14:22 -0700

    Fix production https.

--- back/src/bin/www.js
+++ back/src/bin/www.js
@@ -3,40 +3,53 @@
 /**
  * Module dependencies.
  */
- require('debug');
- const http = require('http');
- 
- require('dotenv').config();
- 
- const app = require('../app.js');
- 
- /**
- * Normalize a port into a number, string, or false.
- */
- const normalizePort = val => {
-   const port = parseInt(val, 10);
-   if (Number.isNaN(port)) {
-     // named pipe
-     return val;
-   }
-   if (port >= 0) {
-     // port number
-     return port;
-   }
-   return false;
- };
- 
- /**
- * Get port from environment and store in Express.
- */
- const port = normalizePort(process.env.PORT || '3000');
- app.set('port', port);
- 
- /**
- * Create HTTP server.
- */
- const server = http.createServer(app);
- 
- server.listen(port, () => {
-   console.log(`Server listening on ${port}`)
- })
+require('debug');
+const http = require('http');
+const https = require('https');
+
+require('dotenv').config();
+
+const app = require('../app.js');
+
+/**
+* Normalize a port into a number, string, or false.
+*/
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+  if (Number.isNaN(port)) {
+    // named pipe
+    return val;
+  }
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+  return false;
+};
+
+/**
+* Get port from environment and store in Express.
+*/
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+var privateKey  = fs.readFileSync('/etc/letsencrypt/live/olta.fr/privkey.pem', 'utf8');
+var certificate = fs.readFileSync('/etc/letsencrypt/live/olta.fr/fullchain.pem', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
+/**
+* Create HTTP server.
+*/
+if (process.env.NODE_ENV !== undefined && process.env.NODE_ENV === 'production') {
+  const server = https.createServer(credentials, app);
+
+  server.listen(port, () => {
+    console.log(`Server https listening on ${port}`)
+  })
+} else {
+  const server = http.createServer(app);
+
+  server.listen(port, () => {
+    console.log(`Server listening on ${port}`)
+  })
+}
